772782824031ff892cdae3f77b4460629946d706
Rafen Sidiq Anggara <rafenrafen1312@gmail.com>
2025-02-21 14:38:55 +0700

    update-3

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,5 +53,5 @@ const sr = ScrollReveal({
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__social-icon, .contact__info',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input, .work__container, .work__title, .work__description, .work__method, .work__result',{interval: 200}); 
